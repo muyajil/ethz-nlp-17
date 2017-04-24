7051e6071e94b6e8399ed7f1a20e5ad86991021d
Gideon Dresdner <gideond@gmail.com>
2017-04-24 15:10:42 +0200

    wip, need to feed_dict

--- lstm.py
+++ lstm.py
@@ -1,37 +1,62 @@
 import tensorflow as tf
 import preprocess
 from itertools import islice
+import numpy as np
 
-h_size = 10
+state_size = 10
 embed_dim = 100
 vocab_size = 20000
 sentence_length = 30
-batch_size = 32
+batch_size = 1
 
-h_cell = tf.contrib.rnn.BasicRNNCell(h_size)
+# h_cell = tf.contrib.rnn.BasicRNNCell(state_size)
 
-Wf = tf.get_variable('Wf', [h_size, embed_dim])
-Uf = tf.get_variable('Uf', [42, h_size]) # TODO
-bf = tf.get_variable('bf', [h_size])
+# Wf = tf.get_variable('Wf', [state_size, embed_dim])
+# Uf = tf.get_variable('Uf', [42, state_size]) # TODO
+# bf = tf.get_variable('bf', [state_size])
 
-def lstm(batch, embedding, H, Wf, Uf, bf):
-    wordvectors = tf.nn.embedding_lookup(embedding, batch)
+# def lstm(batch, embedding, H, Wf, Uf, bf):
+#     wordvectors = tf.nn.embedding_lookup(embedding, batch)
 
-    for i in range(sentence_length-1):
-        curr_word = wordvectors[:,i-1,:]
-        ft = tf.matmul(Wf, tf.transpose(curr_word))
-        ft = tf.transpose(ft)
-        print(ft)
-        ft = tf.matmul(Wf, curr_word) + tf.matmul(Uf, tf.transpose(H)) + bf
+#     for i in range(sentence_length-1):
+#         curr_word = wordvectors[:,i-1,:]
+#         ft = tf.matmul(Wf, tf.transpose(curr_word))
+#         ft = tf.transpose(ft)
+#         print(ft)
+#         ft = tf.matmul(Wf, curr_word) + tf.matmul(Uf, tf.transpose(H)) + bf
 
-sentences = preprocess.iter()
+# sentences = preprocess.iter()
     
+# init = tf.global_variables_initializer()
+# with tf.Session() as sess:
+#     sess.run(init)
+    
+#     embedding = tf.get_variable('embedding', [vocab_size, embed_dim])
+
+#     batch = list(islice(sentences, batch_size))
+
+#     lstm(batch, embedding, tf.get_variable('H', [batch_size, state_size]), Wf, Uf, bf)
+
+
 init = tf.global_variables_initializer()
+lstm = tf.contrib.rnn.BasicLSTMCell(state_size)
+embedding = tf.get_variable('embedding', [vocab_size, embed_dim])
+
+tf.placeholder()
+
+c,h = lstm(v, state)
+
 with tf.Session() as sess:
     sess.run(init)
-    
-    embedding = tf.get_variable('embedding', [vocab_size, embed_dim])
 
-    batch = list(islice(sentences, batch_size))
+    state = tf.zeros([batch_size, state_size])
 
-    lstm(batch, embedding, tf.get_variable('H', [batch_size, h_size]), Wf, Uf, bf)
+    for batch in islice(preprocess.iter(), 10):
+
+        batch = np.array([batch])
+
+        wordvectors = tf.nn.embedding_lookup(embedding, batch)
+
+        v = wordvectors[:,0,:]
+
+
