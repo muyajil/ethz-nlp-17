729f7e87ea8f66187a66588afa8bcbe47370d440
Gideon Dresdner <gideond@gmail.com>
2017-04-24 16:19:05 +0200

    reuse state

--- lstm.py
+++ lstm.py
@@ -3,14 +3,17 @@ import preprocess
 from itertools import islice
 import numpy as np
 
-state_size = 10
+state_size = 512
 embed_dim = 100
 vocab_size = 20000
 sentence_length = 30
-batch_size = 32
+batch_size = 64
 
-memory_state = tf.zeros([batch_size, state_size])
-hidden_state = tf.zeros([batch_size, state_size])
+def get_fresh_state():
+    memory_state = tf.zeros([batch_size, state_size])
+    hidden_state = tf.zeros([batch_size, state_size])
+    return (memory_state, hidden_state)
+    
 init = tf.global_variables_initializer()
 lstm = tf.contrib.rnn.BasicLSTMCell(state_size)
 embedding = tf.get_variable('embedding', [vocab_size, embed_dim])
@@ -21,9 +24,11 @@ with tf.Session() as sess:
         print('batch num: %d' % n_batch)
         wordvectors = tf.nn.embedding_lookup(embedding, batch)
 
+        state = get_fresh_state()
         for i in range(sentence_length-1):
             if i > 0:
                 tf.get_variable_scope().reuse_variables()
                 
-            v = wordvectors[:,i,:]
-            output, state = lstm(v, (memory_state, hidden_state))
+            x = wordvectors[:,i,:]
+            y = wordvectors[:,i+1,:]
+            output, state = lstm(x, state)
