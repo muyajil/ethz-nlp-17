d78dfad9e4304126604be5fd35fa8d6b5fbb22d3
Gideon Dresdner <gideond@gmail.com>
2017-04-24 16:37:20 +0200

    somehow not initializing correctly

--- lstm.py
+++ lstm.py
@@ -14,9 +14,11 @@ def get_fresh_state():
     hidden_state = tf.zeros([batch_size, state_size])
     return (memory_state, hidden_state)
     
-init = tf.global_variables_initializer()
 lstm = tf.contrib.rnn.BasicLSTMCell(state_size)
-embedding = tf.get_variable('embedding', [vocab_size, embed_dim])
+softmax_w = tf.get_variable('softmax_w', [state_size, vocab_size], dtype=tf.float32)
+softmax_b = tf.get_variable('softmax_b', [vocab_size], dtype=tf.float32)
+embedding = tf.get_variable('embedding', [vocab_size, embed_dim],dtype=tf.float32)
+init = tf.global_variables_initializer()
 with tf.Session() as sess:
     sess.run(init)
 
@@ -25,10 +27,16 @@ with tf.Session() as sess:
         wordvectors = tf.nn.embedding_lookup(embedding, batch)
 
         state = get_fresh_state()
+        loss = 0
         for i in range(sentence_length-1):
             if i > 0:
                 tf.get_variable_scope().reuse_variables()
                 
             x = wordvectors[:,i,:]
-            y = wordvectors[:,i+1,:]
+            y = [sentence[i+1] for sentence in batch]
             output, state = lstm(x, state)
+
+            logits = tf.matmul(output, softmax_w) + softmax_b
+            loss += tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y)
+
+        print(sess.run(loss))
