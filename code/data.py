60991e80bb52b063e9078a796e38e0f9d31f1a3a
Mohammed Ajil <muy+github@ajil.ch>
2017-04-25 23:08:36 +0200

    Fix Review from Gideon & Stefan

--- code/data.py
+++ code/data.py
@@ -15,6 +15,7 @@ class NlpData:
     current_batch = 0
 
     def get_next_batch(self, batch_size):
+        """Returns the next batch of samples"""
         lower = self.current_batch * batch_size
         upper = (self.current_batch + 1) * batch_size
         self.current_batch += 1
@@ -30,35 +31,35 @@ class NlpData:
                 temp_data.append(temp_data[upper])
             return temp_data
 
-    def clean_line(self, line_words):
-        line_clean = np.empty(30, dtype="<U50")
+    def __clean_line(self, line_words, seq_length):
+        line_clean = ["" for i in range(seq_length)]
         line_clean[0] = '<bos>'
-        line_clean[29] = '<eos>'
+        line_clean[seq_length-1] = '<eos>'
         num_words = len(line_words)
         for i in range(num_words):
             line_clean[i+1] = line_words[i]
-        for i in range(28-num_words):
+        for i in range((seq_length - 2) - num_words):
             line_clean[i+num_words+1] = '<pad>'
         return line_clean
 
-    def build_vocabulary(self, vocab_size):
-        word_list = self.data.ravel()
+    def __build_vocabulary(self, vocab_size):
+        word_list = [word for sentence in self.data for word in sentence]
         counts = Counter(word_list)
         relevant_counts = counts.most_common(vocab_size-1) # -1 because of <unk>
         word_ids_list = [(relevant_counts[i][0], i) for i in range(len(relevant_counts))]
         word_dict = dict(word_ids_list)
         self.vocab = word_dict
 
-    def process_file(self, file):
+    def __process_file(self, file, seq_length):
         data_list = []
-        for line in file.readlines():
+        for line in file.readlines()[:256]:
             line_words = line.split()
             if len(line_words) < 30:
-                data_list.append(self.clean_line(line_words))
+                data_list.append(self.__clean_line(line_words, seq_length))
         file.close()
-        self.data = np.array(data_list)
+        self.data = data_list
     
-    def generate_data_only_ids():
+    def __generate_data_only_ids(self):
         if self.data is None or self.vocab is None:
             raise InvalidOperationException("The data needs to initialized first")
         temp_data = []
@@ -66,15 +67,16 @@ class NlpData:
             sentence = []
             for word in sentence:
                 sentence.append(self.vocab[word])
-            temp_data.append(np.array(sentence))
-        self.data_only_ids = np.array(temp_data)
+            temp_data.append(sentence)
+        self.data_only_ids = temp_data
 
-    def generate_submission():
+    def generate_submission(self):
+        """Generates submission for testing system"""
         # TODO: generate the submission file
         return
 
-    def __init__(self, data_dir, file_name, vocab_size):
+    def __init__(self, data_dir, file_name, vocab_size, seq_length):
         with open(os.path.join(data_dir, file_name)) as file:
-            self.process_file(file)
-            self.build_vocabulary(vocab_size)
-            self.generate_data_only_ids()
+            self.__process_file(file, seq_length)
+            self.__build_vocabulary(vocab_size)
+            self.__generate_data_only_ids()
--- code/main.py
+++ code/main.py
@@ -12,7 +12,7 @@ DIR = os.path.dirname(os.path.abspath(__file__))
 PARSER = argparse.ArgumentParser(description='LSTM Implementation in Tensorflow')
 
 def main(args):
-    data_train = NlpData(os.path.join(DIR, args.data_dir), args.file_name, args.vocab_size)
+    data_train = NlpData(os.path.join(DIR, args.data_dir), args.file_name, args.vocab_size, args.seq_length)
     batch = data_train.get_next_batch(args.batch_size)
     model = NlpModel(
         args.batch_size, 
@@ -21,10 +21,12 @@ def main(args):
         args.hidden_state_size,
         args.vocab_size,
         args.summaries_dir)
+    # TODO: Train model
+    # TODO: Serialize Data for Tensorboard
     # model.session.run() to access the tf.session variable
 
 if __name__ == '__main__':
-    PARSER.add_argument("--predef", help="Predefined mode for all arguments", type=int)
+    PARSER.add_argument("--predef", help="Predefined mode for all arguments", action='store_true')
     PARSER.add_argument("--data_dir", help="Directory where you keep your data (relative to where your main.py lives)", type=str)
     PARSER.add_argument("--summaries_dir", help="Directory where the summaries for Tensorboard will be stored", type=str)
     PARSER.add_argument("--file_name", help="The file name of the data to parse", type=str)
@@ -35,7 +37,7 @@ if __name__ == '__main__':
     PARSER.add_argument("--vocab_size", help="The size of your vocabulary", type=int)
     args = PARSER.parse_args()
     
-    if args.predef == 1:
+    if args.predef:
         args.data_dir = os.path.join(os.path.dirname(DIR), 'data')
         args.summaries_dir = os.path.join(os.path.dirname(DIR), 'summaries')
         args.file_name = 'sentences.train'
@@ -45,7 +47,8 @@ if __name__ == '__main__':
         args.hidden_state = 512
         args.vocab_size = 20000
     else:
-        if len(list(filter(lambda x: x is None, vars(args).values()))) > 1 and args.predef is None:
+        exists_missing_arg_values = len(list(filter(lambda x: x is None, vars(args).values()))) > 1
+        if exists_missing_arg_values and args.predef is None:
             PARSER.error("Please provide all the needed arguments or use --predef")
         else:
             PARSER.error("You did some weird stuff with the arguments, probably forgot one...")
